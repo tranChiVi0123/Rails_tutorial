cc7d7902026b261190679a7da9ce58c5a5a2c03b
tranChiVi0123 <idiots1230@gmail.com>
2021-01-11 14:14:07 +0700

    Chapter 3: Mostly static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    # @title = "Home page"
   end
 
   def help
+    # @title = "Help page"
+  end
+
+  def about
+    # @title = "About page"
   end
 end
